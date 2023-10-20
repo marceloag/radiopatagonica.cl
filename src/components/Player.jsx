8bd5394dfaaafa30c0b9e8ab1f968c3fd77f3db3
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2023-10-20 10:29:29 -0300

    Player HTML5

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,44 +1,40 @@
-import { useState, useRef } from 'react';
-import ReactAudioPlayer from 'react-audio-player';
+import React, { useState, useRef } from 'react';
 
 function Player() {
-    const audioPlayerRef = useRef(null);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const playAudio = () => {
-      setIsPlaying(true);
-      if (audioPlayerRef.current) {
-        audioPlayerRef.current.audioEl.current.play();
-      }
-    };
+  const audioRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playAudio = () => {
+    setIsPlaying(true);
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
   const pauseAudio = () => {
     setIsPlaying(false);
-     if (audioPlayerRef.current) {
-      audioPlayerRef.current.audioEl.current.pause();
+    if (audioRef.current) {
+      audioRef.current.pause();
     }
   };
 
   return (
-    <>
-    <div class="">
-      <ReactAudioPlayer
-      src="http://34.172.235.38:8000/stream" autoPlay={isPlaying} ref={audioPlayerRef}/>
-    </div>
     <div>
-        {isPlaying ? (<a href="#" onClick={pauseAudio} class="boton rounded-full w-20 h-20 border-4 border-solid border-white  flex flex-row items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="white" stroke-linecap="round" stroke-linejoin="round" class="w-10 h-10">
+      <audio ref={audioRef} src="http://34.172.235.38:8000/stream" autoPlay={isPlaying} />
+      {isPlaying ? (<a href="#" onClick={pauseAudio} className="boton rounded-full w-20 h-20 border-4 border-solid border-white  flex flex-row items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="white" stroke-linecap="round" stroke-linejoin="round" className="w-10 h-10">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
               <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="white"></path>
               <path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="white"></path>
             </svg>
-        </a>) : (<a href="#" onClick={playAudio} class="boton rounded-full w-20 h-20 border-4 border-solid border-white  flex flex-row items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        </a>) : (<a href="#" onClick={playAudio} className="boton rounded-full w-20 h-20 border-4 border-solid border-white  flex flex-row items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
               <path d="M7 4v16l13 -8z"></path>
             </svg>
         </a>)}
     </div>
-    </>
-  )
+  );
 }
 
-export default Player
+export default Player;
