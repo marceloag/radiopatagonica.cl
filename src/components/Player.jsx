a7ee789e47dec838dfcd87b131b968a398804f39
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2024-08-07 15:46:21 -0300

    Player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -20,15 +20,15 @@ function Player() {
 
   return (
     <div>
-      <audio ref={audioRef} src="http://34.172.235.38:8000/stream" autoPlay={isPlaying} />
+      <audio ref={audioRef} autoPlay={isPlaying} src="http://34.172.235.38:8000/stream" type="audio/mpegURL"/>
       {isPlaying ? (<a href="#" onClick={pauseAudio} className="boton rounded-full w-20 h-20 border-4 border-solid border-white  flex flex-row items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="white" stroke-linecap="round" stroke-linejoin="round" className="w-10 h-10">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="white" fill="white" strokeLinecap="round" strokeLinejoin="round" className="w-10 h-10">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="white"></path>
-              <path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="white"></path>
+              <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" strokeWidth="0" fill="white"></path>
+              <path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" strokeWidth="0" fill="white"></path>
             </svg>
         </a>) : (<a href="#" onClick={playAudio} className="boton rounded-full w-20 h-20 border-4 border-solid border-white  flex flex-row items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
               <path d="M7 4v16l13 -8z"></path>
             </svg>
